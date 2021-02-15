4735a377c0503315081ea5a074f26d7e944ed473
markrepedersen <markrepedersen@gmail.com>
2021-02-14 17:36:39 -0800

    static assets

---
+++ dump/2_POPULATE_TEST_DATA.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS ('Mark', 'Pedersen', 'markrepedersen@gmail.com', 'markrp', 'afsf', 'abcd');
+
+INSERT INTO DATASET ('Mark', 'Pedersen', 'markrepedersen@gmail.com', 'markrp', 'afsf', 'abcd');
